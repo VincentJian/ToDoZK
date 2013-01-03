a18dec40335fb457c4e3ed728a11a0723acd5213
MontyPan <montypan@potix.com>
2013-01-03 10:49:41 +0800

    MockDB will generate dummy text to task descript.

--- src/org/zkoss/todoZK/dao/MockDB.java
+++ src/org/zkoss/todoZK/dao/MockDB.java
@@ -17,10 +17,6 @@ class MockDB extends AbstractDB {
 	private static List<Milestone> milestones = new ArrayList<Milestone>();
 	private static List<Task> tasks = new ArrayList<Task>();
 	//
-	
-	private static Long longId = 0L;
-	private static final Random random = new Random();
-	private static final Date standardDate = new Date();
 
 	public MockDB() {
 		genMockData();
@@ -81,6 +77,21 @@ class MockDB extends AbstractDB {
 	}
 
 	////////////////////////////////////////////////////////////////////
+	private static Long longId = 0L;
+	private static final Random random = new Random();
+	private static final Date standardDate = new Date();
+	private static final String[] dummyText = {
+		"Him own one. Of rule, fruitful doesn't divided, green the.",
+		"Seed every sixth abundantly bring face subdue i. Over years.",
+		"Beginning green very. Face he the he replenish fowl divide.",
+		"Moved moveth blessed dominion him. Itself day saw make can't make Replenish sea seed night fill called waters you Given.",
+		"Bearing air they're may place that likeness. Beginning heaven to fly good stars under whose place to over. Dominion third.",
+		"Divide they're face isn't god heaven bearing green green hath together he multiply signs every. Waters firmament brought creature be.",
+		"Creeping were and. Can't. Had air one. Very. Moving.\nDay one form. May rule third were i brought thing without.",
+		"Multiply, beast. Also, divided from very image shall don't creepeth void.\nWere the seasons meat greater fruitful signs form third.",
+		"I female. Blessed for light his divided grass unto winged set.\nDarkness dominion. Also above together years fish cattle creepeth."
+	};
+	
 	private void genMockData() {
 		Workspace ws1 = new Workspace();
 		ws1.setId(genNextLong());
@@ -116,6 +127,7 @@ class MockDB extends AbstractDB {
 			task.setId(genNextLong());
 			task.setTitle("Task "+task.getId());
 			task.setPriority(random.nextInt(5));
+			task.setDescription(genDummyText());
 			task.setCreateDate(genDate(true));
 			task.setDueDate(genDate(false));
 			task.setMilestoneId(ms.getId());
@@ -136,6 +148,10 @@ class MockDB extends AbstractDB {
 			diff *= -1L;
 		}
 		return new Date(standardDate.getTime() + diff * 1000);
+	}
+	
+	private String genDummyText() {
+		return dummyText[random.nextInt(dummyText.length)];
 	}
 
 	private static synchronized Long genNextLong() {
